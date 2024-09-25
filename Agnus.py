0f4f8e4d7d5429a543249d8c9751e8559162a40a
raspberryhelp <agnus1603@gmail.com>
2024-09-24 23:45:12 -0500

    adding my file too ig

---
+++ Agnus.py
@@ -0,0 +1,20 @@
+#importing stuff
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+#making a function because its easier
+def audioconvert(audiopath):
+    y, sr = librosa.load(audiopath) #getting audiopath to load
+    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr) #converts to mel 
+    spectrogramdb = librosa.power_to_db(spectrogram,ref=np.max) #converts to db
+    plt.figure(figsize=(10,4)) #plotting a figure
+    librosa.display.specshow(spectrogramdb,sr=sr,cmap="magma") ##made sure theres no axises and its just this
+    plt.tight_layout() ## gets rid of unnecessary space
+    plt.show() ##shows spectrogram, you can then save from there
+
+
+audioconvert(r"") ##example just paste in the path of the file you want to convert
+##ideally we would want to do this in bulk but haven't figured out how to do that yet
+
