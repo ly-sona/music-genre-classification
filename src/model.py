fd6e74aae3016449d154af3e8bd307ff06bebafb
raspberryhelp <agnus1603@gmail.com>
2024-10-28 13:53:10 -0500

    adding model i guess

---
+++ src/model.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+model = Sequential() ##defining the model
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 1))) #Apply convolution operations to extract features using filters.
+model.add(MaxPooling2D((2, 2))) #Reduce the spatial dimensions, helping to down-sample the input and reduce computational load.
+model.add(Conv2D(64, (3, 3), activation='relu')) #doing it two more times
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten()) #Flattens it to a 1D Vector
+model.add(Dense(128, activation='relu')) #apply classification
+model.add(Dropout(0.5)) # prevents overfitting
+model.add(Dense(n_classes, activation='softmax') ) 
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) ##compling model
+
+X_batch, y_batch = train_generator.__getitem__(0) ##making a batch of data from the data generator class
+
+try:
+    model.fit(train_generator, validation_data=val_generator, epochs=10) ##trying to train
+except Exception as e:
+    print(f"Error {e}")
