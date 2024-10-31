dd64e85324350b01cda663af39ab4cd7ea4c333b
raspberryhelp <agnus1603@gmail.com>
2024-10-30 21:36:20 -0500

    adding import

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from data_generator import *
 
 batch_size = 64
 input_shape = (128, 1024, 1)
