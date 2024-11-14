34f1ec9cfa5208b05982a128c029f0482ea5d63b
bornayo7 <yashbaruah@hotmail.com>
2024-11-13 20:42:57 -0600

    Fixing Navbar positioning

--- aim-project/src/pages/components/Navbar.jsx
+++ aim-project/src/pages/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full px-6 py-4 flex justify-between items-center bg-transparent z-20 relative">
+    <nav className="fixed top-0 left-0 w-full px-6 py-4 flex justify-between items-center bg-transparent z-20">
       {/* Navbar Gradient Background */}
       <div className="absolute top-0 left-0 h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] z-10"></div>
 
